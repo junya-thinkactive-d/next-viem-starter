cb42ce6c90ef1d2be0bee3927f9be1e74bc8137a
junya-thinkactive-d <develop.junya@gmail.com>
2023-11-07 23:06:06 +0900

    :bug: organized hardhat config

--- packages/contract/hardhat.config.ts
+++ packages/contract/hardhat.config.ts
@@ -3,12 +3,12 @@ import '@nomicfoundation/hardhat-toolbox-viem';
 
 const config: HardhatUserConfig = {
   solidity: '0.8.20',
-  networks: {
-    sepolia: {
-      url: process.env.HRADHAT_NETWORKS_URL,
-      accounts: [process.env.PRIVATE_SEPOLIA_ACCOUNT_KEY as string],
-    },
-  },
+  // networks: {
+  //   sepolia: {
+  //     url: process.env.HARDHAT_NETWORKS_URL,
+  //     accounts: [process.env.PRIVATE_SEPOLIA_ACCOUNT_KEY as string],
+  //   },
+  // },
   paths: {
     artifacts: '../client/src/libs/hardhat/artifacts' && './types',
   },
